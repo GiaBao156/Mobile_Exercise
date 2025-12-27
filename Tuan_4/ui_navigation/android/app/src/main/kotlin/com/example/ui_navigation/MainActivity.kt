0638fee4f957e8fb24ee739e456211236e453a8d
GiaBao156 <doanbao690@gmail.com>
2025-12-27 17:07:18 +0700

    Add/update Tuan_4 UI navigation exercise

---
+++ Tuan_4/ui_navigation/android/app/src/main/kotlin/com/example/ui_navigation/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ui_navigation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Tuan_4/ui_navigation/lib/screens/image_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ImageScreen extends StatelessWidget {
+  const ImageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Images')),
+      body: ListView(
+        padding: const EdgeInsets.all(24),
+        children: [
+          Image.network(
+            'https://daotao.ut.edu.vn/wp-content/uploads/2023/10/Hinh-truong-DHGTVT-TPHCM-300x188.jpg',
+          ),
+          const SizedBox(height: 8),
+          const Center(
+            child: Text(
+              'https://daotao.ut.edu.vn/wp-content/uploads/2023/10/Hinh-truong-DHGTVT-TPHCM-300x188.jpg',
+              style: TextStyle(fontSize: 12, color: Colors.blue),
+            ),
+          ),
+          const SizedBox(height: 24),
+          Image.asset('assets/uth.jpg'),
+          const SizedBox(height: 8),
+          const Center(child: Text('In app', style: TextStyle(fontSize: 14))),
+        ],
+      ),
+    );
+  }
+}
